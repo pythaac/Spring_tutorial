9bfcc8dd6d72b41bfcb261685798cf22ee93a8b2
pythaac <pythaac@gmail.com>
2022-01-28 15:21:02 +0900

    initial commit (Spring_tutorial/FCM)

---
+++ FCM/spring/src/test/java/com/pythaac/rememberme/RememberMeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pythaac.rememberme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberMeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
