2e741a71547d2173e3fc8f47fd00e7ec15637f66
pythaac <pythaac@gmail.com>
2022-02-04 20:21:04 +0900

    springBasic initial commit

---
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근기술/src/test/java/com/example/springbasic/SpringBasicApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
