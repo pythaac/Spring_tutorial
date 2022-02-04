3a5590fa909e02c227394c87d362dedc4eca247a
pythaac <pythaac@gmail.com>
2022-02-04 19:55:28 +0900

    chap02 singleton test added

--- 스프링5 프로그래밍 입문/chap02/src/main/java/com/example/chap02/AppContext.java
+++ 스프링5 프로그래밍 입문/chap02/src/main/java/com/example/chap02/AppContext.java
@@ -3,10 +3,16 @@ package com.example.chap02;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class AppContext
-{
+/*
+    1) 스프링은 객체를 생성하고 초기화하는 기능을 제공
+    2) Bean : 스프링이 생성하는 객체
+ */
 
+// @Configuration : 해당 클래스를 스프링 설정 클래스로 지정
+@Configuration
+public class AppContext {
+    // 객체를 생성하고 초기화하는 설정
+    // @Bean : bean 객체에 대한 정보를 담고있는 메서드 -> 해당 메서드가 생성한 객체를 스프링이 관리
     @Bean
     public Greeter greeter(){
         Greeter g = new Greeter();
