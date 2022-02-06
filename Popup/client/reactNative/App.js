d475f1e5c8e964cecf80dedf89079a69cbeebeb2
pythaac <pythaac@gmail.com>
2022-02-07 01:07:13 +0900

    Popup : initial commit

---
+++ Popup/client/reactNative/App.js
@@ -0,0 +1,35 @@
+import { MenuProvider } from 'react-native-popup-menu';
+import {Text, View} from 'react-native'
+import React from 'react';
+
+export const App = () => (
+    <MenuProvider>
+        {YourComponent()}
+    </MenuProvider>
+);
+
+// somewhere in your app
+import {
+    Menu,
+    MenuOptions,
+    MenuOption,
+    MenuTrigger,
+} from 'react-native-popup-menu';
+
+export const YourComponent = () => (
+    <View>
+        <Text>Hello world!</Text>
+        <Menu>
+            <MenuTrigger text='Select action' />
+            <MenuOptions>
+                <MenuOption onSelect={() => alert(`Save`)} text='Save' />
+                <MenuOption onSelect={() => alert(`Delete`)} >
+                    <Text style={{color: 'red'}}>Delete</Text>
+                </MenuOption>
+                <MenuOption onSelect={() => alert(`Not called`)} disabled={true} text='Disabled' />
+            </MenuOptions>
+        </Menu>
+    </View>
+);
+
+export default App;
