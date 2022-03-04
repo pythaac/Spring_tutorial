2fe17a4aafb1fc0616b81627fb590db669c93784
pythaac <pythaac@gmail.com>
2022-03-05 01:37:31 +0900

    springBasic: JdbcMemberRepository added / H2 Database connected

--- 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근기술/src/main/java/com/example/springbasic/SpringConfig.java
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근기술/src/main/java/com/example/springbasic/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.example.springbasic;
 
+import com.example.springbasic.repository.JdbcMemberRepository;
 import com.example.springbasic.repository.MemberRepository;
 import com.example.springbasic.repository.MemoryMemberRepoistory;
 import com.example.springbasic.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepoistory();
+        return new JdbcMemberRepository(dataSource);
     }
 }
